17d9e9b1df8f230556bdbd6e9a14d2e86105eafe
Matt Russo <nmarusso@amazon.com>
2024-09-26 12:29:07 -0400

    add divider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react'
-import Button from '@mui/material/Button';
 import './App.css'
+//components
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+//table
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +18,18 @@ function App() {
   const thumbnail = document.querySelectorAll(".thumbnail")
   console.log(thumbnail);
 
-  // setCount > 0 ? 
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+  
 
   return (
     // <html><body><h1>Welcome to Cloud Castle Dev</h1><p>Testing site for AWS training</p><img src='https://img-38se4389gs9838pfsc.s3.amazonaws.com/magic.gif' alt='magic meme' style='width:240px;height:240px;'></body></html>
@@ -21,6 +42,40 @@ function App() {
         </Button>
         <img className='thumbnail' src="https://img-38se4389gs9838pfsc.s3.amazonaws.com/magic.gif" alt="'magic' gif" style={{ visibility: count > 0 ? 'visible': 'hidden'}} />
         <p>Testing site for AWS training</p>
+      </div>
+      
+      <Divider variant="middle" style={{ backgroundColor: "#fff" }} />
+
+      <div className="card">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </>
   )
