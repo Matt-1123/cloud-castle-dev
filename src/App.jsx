3f6ab9c603f03298a7f08fbd1b1112cfd7ba7752
Matt Russo <nmarusso@amazon.com>
2024-12-19 14:45:19 -0500

    deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,6 @@ function App() {
   const thumbnail = document.querySelectorAll(".thumbnail")
   console.log(thumbnail);
 
-  
-  
-  
-  
-
   return (
     // <html><body><h1>Welcome to Cloud Castle Dev</h1><p>Testing site for AWS training</p><img src='https://img-38se4389gs9838pfsc.s3.amazonaws.com/magic.gif' alt='magic meme' style='width:240px;height:240px;'></body></html>
     
