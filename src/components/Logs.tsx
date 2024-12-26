edb07a3ee445009380ba5fb00712b6bd06d34dc6
Matt Russo <nmarusso@amazon.com>
2024-12-26 14:10:29 -0500

    list test logs

--- src/components/Logs.tsx
+++ src/components/Logs.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 import type { Schema } from '../../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
 
 const client = generateClient<Schema>()
 
-export default function TodoList() {
+export default function LogList() {
+  const [logs, setLogs] = useState<Schema["Log"]["type"][]>([]);
+
+  const fetchLogs = async () => {
+    const { data: items, errors } = await client.models.Log.list();
+    setLogs(items);
+  };
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
+  
   const createLog = async () => {
     await client.models.Log.create({
       content: window.prompt("Log content?")
     })
   }
 
-  return (
+  fetchLogs();
+
+  return (<>
     <Button onClick={createLog}>Submit Log</Button>
-  )
+    <ul>
+      {logs.map(({ id, content }) => (
+        <li key={id}>{content}</li>
+      ))}
+    </ul>
+  </>)
 }
