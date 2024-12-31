05e188cd77bae7ece9c2a2f6a1d3a648b03102d0
Matt Russo <matthew.russo91@gmail.com>
2024-12-30 21:52:09 -0500

    add email authentication

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+// import type { AppProps } from 'next/app';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../../amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  
+Amplify.configure(outputs);
 
+export default function App({ Component, pageProps }) {
   return (
-    // <html><body><h1>Welcome to Cloud Castle Dev</h1><p>Testing site for AWS training</p><img src='https://img-38se4389gs9838pfsc.s3.amazonaws.com/magic.gif' alt='magic meme' style='width:240px;height:240px;'></body></html>
-    
-    <h1>Register</h1>
-  )
-}
-
-export default App
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Component {...pageProps} />
+        </main>
+      )}
+    </Authenticator>
+  );
+};
