a34483a8a4a59195367b1711f86ca469d539817c
Matt Russo <matthew.russo91@gmail.com>
2024-12-31 23:54:15 -0500

    remove login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 // import Header from './components/Header'
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
           {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
